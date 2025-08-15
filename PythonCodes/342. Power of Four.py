146cbdc1075a8cdc9894c9f4234b0831e9f1a577
Anghelescu Radu Mihai <94002877+radu1265@users.noreply.github.com>
2025-08-15 19:15:47 +0300

    Update 342. Power of Four.py

--- PythonCodes/342. Power of Four.py
+++ PythonCodes/342. Power of Four.py
@@ -8,3 +8,13 @@ class Solution:
         if count % 2:
             return True
         return False
+'''
+faster O(1) instead of O(log4X)
+n & (n - 1) checks if number is power of 2
+n & 0xAAAAAAAA checks if this is power of 2 (needs the first check)
+0xAAAAAAAA = 10101010101010101010101010101010
+works for 32 bits integers
+return (n != 0 and 
+          ((n & (n - 1)) == 0) and 
+            not(n & 0xAAAAAAAA))
+'''
