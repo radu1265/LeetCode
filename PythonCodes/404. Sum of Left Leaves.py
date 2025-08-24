3b8b52637b55b19b22fe1ff4b3f845826bfbc383
Anghelescu Radu Mihai <94002877+radu1265@users.noreply.github.com>
2025-08-24 23:29:17 +0300

    Create 404. Sum of Left Leaves.py

---
+++ PythonCodes/404. Sum of Left Leaves.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+
+        queue = [root]
+        out = 0
+        while queue:
+            node = queue.pop(0)
+            if node.left:
+                queue.append(node.left)
+                if not node.left.left and not node.left.right:
+                    out += node.left.val
+            if node.right:
+                queue.append(node.right)
+        return out  
+        
+        
