badec763ca6437102f646e2388fab4cba6f3ebdc
Anghelescu Radu Mihai <94002877+radu1265@users.noreply.github.com>
2025-10-06 11:35:11 +0300

    iterative method added
    Refactor DFS method for leaf comparison in trees.

--- PythonCodes/872. Leaf-Similar Trees.py
+++ PythonCodes/872. Leaf-Similar Trees.py
@@ -14,3 +14,34 @@ class Solution:
 
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         return self.DFS(root1) == self.DFS(root2)
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def DFS_leafSimilar(self, node: Optional[TreeNode]) -> List[int]:
+#         visited = set()
+#         nodes = [node]
+#         leaf_seq = []
+#         while nodes:
+            
+#             node = nodes.pop()
+#             if node in visited:
+#                 cotinue
+
+#             visited.add(node)
+#             if node.right and node.right not in visited:
+#                 nodes.append(node.right)
+
+#             if node.left and node.left not in visited:
+#                 nodes.append(node.left)
+
+#             if not node.left and not  node.right:
+#                 leaf_seq.append(node.val)
+
+#         return leaf_seq
+
+#     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+#         return self.DFS_leafSimilar(root1) == self.DFS_leafSimilar(root2)
